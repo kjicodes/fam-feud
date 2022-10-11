8ff2def7f384f39c4079214d88706ebacb859f79
KI <kateleenissa@theMother.local>
2022-10-11 10:49:11 -0400

    Edit round buttons components

--- src/App.js
+++ src/App.js
@@ -5,23 +5,24 @@ import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Question from './components/Question/Question';
 import Cards from './components/Cards/Cards';
-import RoundButton from './components/RoundButton/RoundButton';
+import RoundButtons from './components/RoundButtons/RoundButtons';
 
 export default class App extends Component {
 
   state = {
     round: 1,
-    team: ['Team1', 'Team2'],
+    team: ['', ''],
     currTeam: '',
     question: "NAME SONGS WITH THE WORD 'LOVE' IN THE TITLE", // 5 questions in total = 5 rounds?
     scores: [0, 0], // scores for each team (each family) 
     card: '', // word behind each card
-    wrongAnswer: 0,
-    showCard: [false, false, false, false], // to show the card if guess is correct
+    isWrong: 0,
+    showCard: [false, false, false, false], // 4 answer cards - to show the card if guess is correct, start false
     showX: false, // X's shown for wrong guesses, 3 attempts
     roundScore: 0
   }
 
+  update
 
 
   render() {
@@ -35,11 +36,7 @@ export default class App extends Component {
         />
         <Question question={this.state.question} />
         <Cards />
-        <RoundButton round={1} />
-        <RoundButton round={2} />
-        <RoundButton round={3} />
-        <RoundButton round={4} />
-        <RoundButton round={5} />
+        <RoundButtons />
       </div>
     );
   }
