35041e6067623086e5fa36b9aec028dda1420dea
deegyi <dee_gyi@live.ca>
2022-10-13 17:00:22 -0400

    update2

--- src/App.js
+++ src/App.js
@@ -7,63 +7,41 @@ import HomePage from "./pages/HomePage/HomePage";
 import NewTeamPage from "./pages/NewTeamPage/NewTeamPage";
 
 export default class App extends Component {
-  render() {
-    state = {
-      user: null,
-    };
+  state = {
+    user: null,
+  };
 
-    this.componentDidMount = () => {
-      let token = localStorage.getItem("token");
-      if (token) {
-        const payload = JSON.parse(atob(token.split(".")[1]));
-        if (payload.exp < Date.now() / 2000) {
-          localStorage.removeItem("token");
-          token = null;
-        } else {
-          let userDoc = payload.user;
-          this.setState({ user: userDoc });
-        }
+  componentDidMount = () => {
+    let token = localStorage.getItem("token");
+    if (token) {
+      const payload = JSON.parse(atob(token.split(".")[1]));
+      if (payload.exp < Date.now() / 2000) {
+        localStorage.removeItem("token");
+        token = null;
+      } else {
+        let userDoc = payload.user;
+        this.setState({ user: userDoc });
       }
-    };
+    }
+  };
 
-    setUserInState = (incomingUserData) => {
-      this.setState({ user: incomingUserData });
-    };
+  setUserInState = (incomingUserData) => {
+    this.setState({ user: incomingUserData });
+  };
 
+  render() {
     return (
       <div className="App">
-        <nav className="navbar">
-          <ul className="nav-link">
-            <li className="title">We're All Related Feud</li>
-            <li>
-              <Link to="/warfeud/game" className="game">
-                Play Game
-              </Link>
-            </li>
-            <li>
-              <Link to="/warfeud" className="game">
-                Home
-              </Link>
-            </li>
-            <li className="dropdown">
-              <button className="dropbtn">Account</button>
-
-              <div className="dropdown-content">
-                <li>
-                  <Link to="/warfeud">Log Out</Link>
-                </li>
-              </div>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/warfeud" element={<HomePage />} />
-          <Route path="/warfeud/new" element={<NewTeamPage />} />
-          <Route path="/warfeud/game" element={<GamePage />} />
-          <Route path="*" element={<Navigate to="/warfeud" replace />} />
-        </Routes>
-        :
-        <AuthPage setUserInState={this.setUserInState} />
+        {this.state.user ? (
+          <Routes>
+            <Route path="/warfeud" element={<HomePage />} />
+            <Route path="/warfeud/new" element={<NewTeamPage />} />
+            <Route path="/warfeud/game" element={<GamePage />} />
+            <Route path="*" element={<Navigate to="/warfeud" replace />} />
+          </Routes>
+        ) : (
+          <AuthPage setUserInState={this.setUserInState} />
+        )}
       </div>
     );
   }
